ef5c8f403e013112b0b6131564a10a25ee7ee3c1
Marina <marinafaenze@hotmail.com>
2020-04-14 18:10:03 -0300

    function buscarGeneros and module.exports buscarGeneros

--- servidor/controladores/controladorGeneros.js
+++ servidor/controladores/controladorGeneros.js
@@ -1,1 +1,15 @@
 const conexion = require('../lib/conexionbd');
+
+function buscarGeneros(require, response){
+    let sql = "SELECT * FROM genero";
+    conexion.query(sql, (err, resultado, fields)=>{
+        if(err){
+            console.log("Hubo un error en la consulta", error.message);
+            return require.status(404).send("Hubo un error en la consulta");
+        }
+        var respuesta = { 'generos' : resultado}
+        response.send(respuesta);
+    });
+}
+
+module.exports = { buscarGeneros : buscarGeneros}

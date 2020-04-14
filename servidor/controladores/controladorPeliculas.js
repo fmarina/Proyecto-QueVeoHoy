64e3acc11d05335db7bbee35304589f7eed4ed6d
Marina <marinafaenze@hotmail.com>
2020-04-14 15:23:36 -0300

    function buscarPeliculas, module buscarPeliculas exports

--- servidor/controladores/controladorPeliculas.js
+++ servidor/controladores/controladorPeliculas.js
@@ -1,2 +1,16 @@
 const conexion = require('../lib/conexionbd');
 
+function buscarPeliculas(require, response){
+    let sql = "SELECT * FROM pelicula";
+    conexion.query(sql, (err, resultado, fields)=>{
+        if(err){
+            console.log("Hubo un error en la consulta", error.message);
+            return require.status(404).send("Hubo un error en la consulta");
+        }
+        var respuesta = { 'peliculas' : resultado}
+        response.send(respuesta);
+    });
+}
+
+module.exports = { buscarPeliculas : buscarPeliculas}
+

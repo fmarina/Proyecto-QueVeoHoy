bf146ed711756cb32ef8a0de96dde6fd1eb7525e
Marina <marinafaenze@hotmail.com>
2020-04-14 22:44:19 -0300

    create function filtrarPeliculas(anio, titulo, genero)

--- servidor/controladores/controladorPeliculas.js
+++ servidor/controladores/controladorPeliculas.js
@@ -9,18 +9,8 @@ function buscarPeliculas(require, response){
     let pagina = require.query.pagina;
     let cantidad = require.query.cantidad;
    
-    var sql = "SELECT * FROM pelicula";
-    var query = "";
-    if(anio || titulo || genero){
-        query = " where ";
-        if(anio) query = query + " anio = " + anio + " and"; 
-        if(titulo) query = query + " titulo like '%" + titulo + "%'" + " and";    
-        if(genero) query = query + " genero_id = " + genero + " and" ;  
-
-        var query1 = query.split(' ').slice(0, -1).join(' ');     
-        query = query1;     
-    }
-    
+    let sql = "SELECT * FROM pelicula";
+    let query = filtrarPeliculas(anio, titulo, genero);
     sql = sql + query;
 
     conexion.query(sql, (err, resultado, fields)=>{
@@ -29,9 +19,22 @@ function buscarPeliculas(require, response){
             return require.status(404).send("Hubo un error en la consulta");
         }
         console.log(resultado);
-        var respuesta = { 'peliculas' : resultado}
+        const respuesta = { 'peliculas' : resultado}
         response.send(respuesta);
     });
+}
+
+const filtrarPeliculas = (anio, titulo, genero) =>{
+    let query = "";
+    if(anio || titulo || genero){
+        query = " where ";
+        if(anio) query = query + " anio = " + anio + " and"; 
+        if(titulo) query = query + " titulo like '%" + titulo + "%'" + " and";    
+        if(genero) query = query + " genero_id = " + genero + " and" ;
+        
+        query = query.split(' ').slice(0, -1).join(' ');                  
+    }
+    return query;
 }
 
 module.exports = { buscarPeliculas : buscarPeliculas}

ff618bdfe2ef4109a96d9332d3837e2f56a51f57
Marina <marinafaenze@hotmail.com>
2020-04-16 11:49:34 -0300

    buscarRecomendacion() function completed

--- servidor/controladores/controladorRecomendaciones.js
+++ servidor/controladores/controladorRecomendaciones.js
@@ -4,7 +4,18 @@ function buscarRecomendacion(require, response){
     const genero = require.query.genero;
     const anio_inicio = require.query.anio;
     const anio_fin = require.query.anio_lanzamiento;
-    const puntuacion = require.query.puntuacion;    
+    const puntuacion = require.query.puntuacion;  
+    let sql = "SELECT * FROM pelicula join genero on genero_id = genero.id";
+    let query = filtroRecomendacion(genero, anio_inicio, anio_fin, puntuacion);
+    sql = sql + query;
+    conexion.query(sql, (err, result)=>{
+        if(err) {
+            console.log("Hubo un error en la consulta sql recomendacion", err.message);
+            return response.status(404).send("Hubo un error en la consulta sql recomendacion");
+        }
+        const respuesta = { 'peliculas' : result}
+        response.send(respuesta);
+    });  
 }
 
 const filtroRecomendacion = (genero, inicio, fin, puntuacion)=>{    

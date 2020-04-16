2f5f5ab3abfd9027214724a36fbd3c513544cbe3
Marina <marinafaenze@hotmail.com>
2020-04-16 17:28:15 -0300

    refactoring filtroRecomendaciones.js

--- servidor/controladores/controladorRecomendaciones.js
+++ servidor/controladores/controladorRecomendaciones.js
@@ -2,8 +2,8 @@ const conexion = require('../lib/conexionbd');
 
 function buscarRecomendacion(require, response){
     const genero = require.query.genero;
-    const anio_inicio = require.query.anio;
-    const anio_fin = require.query.anio_lanzamiento;
+    const anio_inicio = require.query.anio_inicio;
+    const anio_fin = require.query.anio_fin;
     const puntuacion = require.query.puntuacion;  
     let sql = "SELECT * FROM pelicula join genero on genero_id = genero.id";
     let query = filtroRecomendacion(genero, anio_inicio, anio_fin, puntuacion);
@@ -13,6 +13,7 @@ function buscarRecomendacion(require, response){
             console.log("Hubo un error en la consulta sql recomendacion", err.message);
             return response.status(404).send("Hubo un error en la consulta sql recomendacion");
         }
+        console.log(sql);
         const respuesta = { 'peliculas' : result}
         response.send(respuesta);
     });  
@@ -22,10 +23,9 @@ const filtroRecomendacion = (genero, inicio, fin, puntuacion)=>{
     let query = "";
     if(genero || inicio || fin || puntuacion){
         query = " WHERE "
-        if(genero) query = query + " genero.nombre = '" + genero + "' AND";
-        if(inicio) query = query + " anio = " + inicio + " AND";
-        if(fin) query = query + " anio_lanzamiento = " + fin + " AND";
-        if(puntuacion) query = query + " puntuacion = " + puntuacion + " AND";
+        if(genero) query = `${query} genero.nombre = '${genero}' AND` ;
+        if(inicio || fin) query = `${query} anio BETWEEN ${inicio} AND ${fin} AND`; 
+        if(puntuacion) query = `${query} puntuacion = ${puntuacion} AND"`;
         query = query.split(' ').slice(0, -1).join(' ');
     }
     return query;
